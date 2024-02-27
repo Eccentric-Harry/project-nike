d51545f66e64860184b7077123e788836b056385
Eccentric-Harry <139021195+Eccentric-Harry@users.noreply.github.com>
2024-02-27 13:32:42 +0530

    Add files via upload

---
+++ src/sections/index.js
@@ -0,0 +1,14 @@
+import Hero from "./Hero";
+
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+
+export
+{
+    Hero,  Footer, CustomerReviews, PopularProducts, Services, Subscribe, SpecialOffers, SuperQuality
+}
